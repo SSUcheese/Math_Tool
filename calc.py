fc9e71f812f341747e8b3877a1ffeee8bd5447b7
cheese <zoojj3319@gmail.com>
2022-06-29 21:30:46 +0900

    Fix

--- calc.py
+++ calc.py
@@ -9,12 +9,8 @@ def subtract(a, b):
 
 
 def multiply(a, b):
-<<<<<<< HEAD
     return a * b
 
 
 def divide_free(a, b):
     return a/b
-=======
-    return a*b
->>>>>>> 5f8691caad62b364d0bcf5054b3929e8476e6882
